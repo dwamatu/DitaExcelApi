c78027d83b68c91b921b70e4f97cd4f7928dd12b
mtotomgonjwa <mtoto.mgonjwa@gmail.com>
2017-05-19 17:08:15 +0300

    Updated BaseController Refactored File Functions

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -73,68 +73,6 @@ class BaseController extends Controller
 
     //</editor-fold>
 
-    public function saveFileType(Request $request)
-    {
-        $newFileTypeData = $request->all();
-
-        $type = $this->createType($newFileTypeData);
-
-        return $type;
-    }
-
-    public function saveFile(Request $request)
-    {
-        //Validate requests
-        $this->validate($request, [
-            'file' => 'required',
-        ]);
-
-        $resource = $request->file('file');
-
-
-        $filetype = $request->file('file')->getClientOriginalExtension();
-        $filename = $request->file('file')->getClientOriginalName();
-        //Store File
-        FileUtilities::storeFile($resource,$filename);
-
-        //Check File Type Exists and Create if Does'nt Create a File Type.
-        $paramType = Type::firstOrCreate(['name' => $filetype] );
-        //Store File Details
-        $file = $this->storeFileMetadata($filename, $paramType);
-
-        return $file;
-    }
-
-    /**
-     * @param $newFileTypeData
-     * @return Type
-     */
-    private function createType($newFileTypeData)
-    {
-        $type = new Type();
-
-        $type->name = $newFileTypeData['name'];
-
-        $type->save();
-
-        return $type;
-    }
-
-    /**
-     * @param $filename
-     * @param $paramType
-     * @return File
-     */
-    private function storeFileMetadata($filename, $paramType)
-    {
-        $file = new File();
-
-        $file->file_name = $filename;
-        $file->file_type = $paramType->id;
-
-        $file->save();
-        return $file;
-    }
 
 
 }
