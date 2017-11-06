95326b3d29061bfc7b9c13c4dca19ff5b80bd3aa
Michael Bukachi <michaelbukachi@gmail.com>
2017-11-06 10:19:44 +0300

    Added custom webpack configuration

--- resources/assets/js/components/ViewPapers.vue
+++ resources/assets/js/components/ViewPapers.vue
@@ -15,26 +15,36 @@
         </div>
         <div>
             <table class="table table-bordered table-hover table-striped table-responsive">
+                <thead>
                 <tr>
-                    <th class="text-center">Name</th>
+                    <th class="text-center" @click="ascending = !ascending">Name
+                        <icon name="glyphicon-triangle-top"></icon>
+                    </th>
                     <th class="text-center">Type</th>
                     <th class="text-center">Semester</th>
                     <th class="text-center">File</th>
                 </tr>
-                <tr v-for="paper in papers">
+                </thead>
+
+                <tbody>
+                <tr v-for="paper in ascending ? sortAscending : sortDescending">
                     <td class="text-center">{{ paper.name }}</td>
                     <td class="text-center">{{ paper.resource_type.toUpperCase() }}</td>
                     <td class="text-center">{{ paper.semester }}</td>
-                    <td class="text-center"><a href="#">Download</a></td>
+                    <td class="text-center"><a v-bind:href="'/file/name/' + paper.file">Download</a></td>
                 </tr>
+                </tbody>
+
             </table>
         </div>
         <div class="pagination-section text-center">
             <ul class="pagination">
-                <li v-bind:class="{disabled: currentPage == 1}" @click="setPage(1)"><a>First</a></li>
+                <li v-bind:class="{disabled: currentPage == 1 || totalPages == 0 }" @click="setPage(1)"><a>First</a>
+                </li>
                 <li v-for="page in totalPages" v-bind:class="{active: page == currentPage}" @click="setPage(page)">
                     <a>{{ page }}</a></li>
-                <li v-bind:class="{disabled: currentPage == totalPages}" @click="setPage(totalPages)"><a>Last</a></li>
+                <li v-bind:class="{disabled: currentPage == totalPages || totalPages == 0}"
+                    @click="setPage(totalPages)"><a>Last</a></li>
             </ul>
         </div>
     </div>
@@ -46,6 +56,9 @@
     const STATUS_INITIAL = 0, STATUS_FETCHING = 1, STATUS_SUCCESS = 2, STATUS_FAILED = 3;
     let currentPage = 1;
     export default {
+        components: {
+            "icon": require("vue-icons")
+        },
         name: 'app',
         data() {
             return {
@@ -53,7 +66,8 @@
                 currentStatus: null,
                 currentPage: null,
                 totalPages: null,
-                papers: null
+                papers: [],
+                ascending: true
             }
         },
         computed: {
@@ -68,6 +82,28 @@
             },
             isFailed() {
                 return this.currentStatus === STATUS_FAILED;
+            },
+            sortAscending() {
+                return this.papers.sort(function (a, b) {
+                    if (a.name < b.name) {
+                        return -1;
+                    } else if (a.name > b.name) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                })
+            },
+            sortDescending() {
+                return this.papers.sort(function (a, b) {
+                    if (a.name > b.name) {
+                        return -1;
+                    } else if (a.name < b.name) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                })
             }
         },
         methods: {
@@ -87,7 +123,7 @@
             setPage(page) {
                 this.currentPage = page;
                 this.fetchPapers();
-            }
+            },
         },
         mounted() {
             this.fetchPapers()
